10b0b21db650e43eec7a87d63e23646e4cc61ed3
Zalilov Marat <Zalilov Marat@DESKTOP-BMGRAJ6>
2023-12-04 22:55:58 +0300

    Добавьте файлы проекта.

---
+++ ProgramTest/GameForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ProgramTest
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.MainButton = new System.Windows.Forms.Button();
+            this.SecondaryPanel = new System.Windows.Forms.Panel();
+            this.MainLabelText = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            this.SecondaryPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(8)))), ((int)(((byte)(24)))));
+            this.MainPanel.Controls.Add(this.MainButton);
+            this.MainPanel.Controls.Add(this.SecondaryPanel);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(800, 450);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // MainButton
+            // 
+            this.MainButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainButton.Location = new System.Drawing.Point(339, 326);
+            this.MainButton.Name = "MainButton";
+            this.MainButton.Size = new System.Drawing.Size(155, 50);
+            this.MainButton.TabIndex = 1;
+            this.MainButton.Text = "Начать";
+            this.MainButton.UseVisualStyleBackColor = true;
+            this.MainButton.Click += new System.EventHandler(this.MainButton_Click);
+            
+            // 
+            // SecondaryPanel
+            // 
+            this.SecondaryPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(24)))), ((int)(((byte)(70)))));
+            this.SecondaryPanel.Controls.Add(this.MainLabelText);
+            this.SecondaryPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SecondaryPanel.Location = new System.Drawing.Point(0, 0);
+            this.SecondaryPanel.Name = "SecondaryPanel";
+            this.SecondaryPanel.Size = new System.Drawing.Size(800, 100);
+            this.SecondaryPanel.TabIndex = 0;
+            // 
+            // MainLabelText
+            // 
+            this.MainLabelText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainLabelText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainLabelText.ForeColor = System.Drawing.Color.White;
+            this.MainLabelText.Location = new System.Drawing.Point(0, 0);
+            this.MainLabelText.Name = "MainLabelText";
+            this.MainLabelText.Size = new System.Drawing.Size(800, 100);
+            this.MainLabelText.TabIndex = 0;
+            this.MainLabelText.Text = "Главное меню";
+            this.MainLabelText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "GameForm";
+            this.Text = "Программа которая в разработке";
+            this.MainPanel.ResumeLayout(false);
+            this.SecondaryPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Panel SecondaryPanel;
+        private System.Windows.Forms.Label MainLabelText;
+        private System.Windows.Forms.Button MainButton;
+    }
+}
+
---
+++ ProgramTest/GameForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ProgramTest
+{
+    public partial class GameForm : Form
+    {
+        private System.Windows.Forms.Button[] buttons;
+        private GameForm gameForm;
+        
+
+        private string[] NameButton = {"Buttons0", "Buttons1", "Buttons2"};
+
+        private bool _isButton= false;
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainButton_Click(object sender, EventArgs e)
+        {
+            MainPanel.Controls.Remove(MainButton);
+            
+            ButtonGeneration();
+
+        }
+        private void Buttons_Click(object sender, EventArgs e)
+        {
+            
+            
+
+            QuestionAndAnswerGeneration(sender,e);
+
+
+        }
+        //private void Buttons_MouseClick(object sender, MouseEventArgs e)
+        //{
+
+        //    if (e.Button == MouseButtons.Left)
+        //    {
+        //        _isButton = e.Equals(sender) ;
+        //        QuestionAndAnswerGeneration(sender, e);
+        //    }
+
+                
+
+
+        //}
+        public void ButtonGeneration()
+        {
+            
+            buttons = new System.Windows.Forms.Button[3];
+            int W = 330, H = 200;
+            for (int j = 0; j < 3; j++)
+            {
+                buttons[j] = new System.Windows.Forms.Button
+                {
+                    BackColor = Color.Orange,
+                    Size = new Size(200, 50),
+                    Location = new Point(W, H += 50),
+                    Text = $"{j+1} "+ $"Ответ"
+                };
+                buttons[j].Name = NameButton[j];
+                buttons[j].Click += new EventHandler(this.Buttons_Click);
+                buttons[j].Tag = new Point(j);
+                MainPanel.Controls.Add(buttons[j]);
+                
+
+            }
+        }
+        public void QuestionAndAnswerGeneration(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(@"C:\Users\Zalilov Marat\source\repos\ProgramTest\ProgramTest\QuestionAndAnswer\QuestionAndAnswer.txt");
+                string line = sr.ReadLine();
+
+                //while (line != null)
+                //{
+                    MainLabelText.Text = line;
+                    line = sr.ReadLine();
+                    for(int j = 0;j < 3; j++)
+                    {
+                        if ( NameButton[j] == "Buttons0")
+                        {
+                            MessageBox.Show("Работает 1 кнопка");
+                            MainLabelText.Text = line;
+                            line = sr.ReadLine();
+                            
+                        }
+                        else if (NameButton[j] == "Buttons1")
+                        {
+                            MessageBox.Show("Работает 2 кнопка");
+                            MainLabelText.Text = line;
+                            line = sr.ReadLine();
+                            
+                        }
+                        else if (NameButton[j] == "Buttons2")
+                        {
+                            MessageBox.Show("Работает 3 кнопка");
+                            MainLabelText.Text = line;
+                            line = sr.ReadLine();
+                            
+                        }
+                    }
+                    
+                    
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+    }
+}
